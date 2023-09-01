8667358d50e0cb26df531e34f4e1657cfaa6693e
John James <john.james.ai.studio@gmail.com>
2023-09-01 03:57:38 -0400

    bmp

--- studioai/data/dataset.py
+++ studioai/data/dataset.py
@@ -11,7 +11,7 @@
 # URL        : https://github.com/john-james-ai/studioai                                           #
 # ------------------------------------------------------------------------------------------------ #
 # Created    : Thursday August 10th 2023 08:29:08 pm                                               #
-# Modified   : Friday September 1st 2023 03:54:13 am                                               #
+# Modified   : Friday September 1st 2023 03:57:19 am                                               #
 # ------------------------------------------------------------------------------------------------ #
 # License    : MIT License                                                                         #
 # Copyright  : (c) 2023 John James                                                                 #
@@ -24,7 +24,7 @@ from typing import Callable, Union, List
 import pandas as pd
 
 from studioai.stats.descriptive.summary import SummaryStats
-from studioai.visual.base import Visualizer
+from studioai.visual.seaborn import Visualizer
 
 # ------------------------------------------------------------------------------------------------ #
 logger = logging.getLogger(__name__)
